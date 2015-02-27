63dbc386ed9799ee4183f7fed2081677c85021d0
Dan Lew <danlew42@gmail.com>
2015-02-27 12:25:30 -0600

    Got sample working, cleared out unnecessary bits

--- sample/src/main/java/com/trello/victorsample/MainActivity.java
+++ sample/src/main/java/com/trello/victorsample/MainActivity.java
@@ -1,9 +1,8 @@
 package com.trello.victorsample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,27 +10,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView secondImageView = (ImageView) findViewById(R.id.second_image_view);
+        secondImageView.setImageResource(R.drawable.ic_smartphone);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
