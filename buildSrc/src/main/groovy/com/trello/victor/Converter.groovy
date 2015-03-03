948080e8fb2c3b42f6fe4b75e62353830f5457b8
Dan Lew <danlew42@gmail.com>
2015-03-03 08:39:35 -0600

    Split out conversion into its own testable class
    It is too difficult to test a Task when all I want to do is test
    what it does. This is a clean separation between action and task
    inputs.

---
+++ buildSrc/src/main/groovy/com/trello/victor/Converter.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2015 Trello, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.trello.victor
+
+import org.apache.batik.transcoder.Transcoder
+import org.apache.batik.transcoder.TranscoderInput
+import org.apache.batik.transcoder.TranscoderOutput
+import org.apache.batik.transcoder.image.ImageTranscoder
+import org.apache.batik.transcoder.image.PNGTranscoder
+
+/**
+ * Converts SVGs to PNGs.
+ *
+ * This is split out into its own class to make it easier to test (since it doesn't require
+ * any of the Task architecture).
+ */
+class Converter  {
+
+    private Transcoder transcoder = new PNGTranscoder();
+
+    void transcode(File svgFile, Density density, int baseDpi, File destination) {
+        float pixelUnitToMillimeter = (2.54f / (baseDpi * density.multiplier)) * 10
+        transcoder.addTranscodingHint(ImageTranscoder.KEY_PIXEL_UNIT_TO_MILLIMETER,
+                new Float(pixelUnitToMillimeter));
+
+        String svgURI = svgFile.toURI().toString();
+        TranscoderInput input = new TranscoderInput(svgURI);
+
+        OutputStream outStream = new FileOutputStream(destination)
+        TranscoderOutput output = new TranscoderOutput(outStream);
+
+        transcoder.transcode(input, output);
+
+        outStream.flush();
+        outStream.close();
+    }
+}
