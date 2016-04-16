286228e1db2acf61cf9d1dea0edaee29b42b395d
Romain Piel <romain.piel@gmail.com>
2016-04-16 13:42:32 +0100

    throw an exception when the SVG to vector drawable failed

--- victor/src/main/groovy/com/trello/victor/SVG2AndroidTask.groovy
+++ victor/src/main/groovy/com/trello/victor/SVG2AndroidTask.groovy
@@ -2,6 +2,7 @@ package com.trello.victor
 
 import com.romainpiel.svgtoandroid.Svg2Vector
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputDirectory
@@ -47,6 +48,7 @@ class SVG2AndroidTask extends DefaultTask {
             String error = Svg2Vector.parseSvgToXml(svgFile, outStream)
             if (!error.isEmpty()) {
                 logger.error(error)
+                throw new GradleException(error)
             }
 
             outStream.flush()
